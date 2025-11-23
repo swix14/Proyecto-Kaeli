497c50886bd183c254a5e92e4b5b615394b363fa
Sebastian Victoriano <sebastianvictoriano14@gmail.com>
2025-11-23 00:52:37 -0300

    Revert "Develop"

--- kaeli-mvp/app/models/__init__.py
+++
@@ -1,8 +0,0 @@
-from .usuario import Usuario
-from .carrito import CarritoItem
-from .resena import Resena, tabla_votos
-from app import db
-from .usuario import Usuario
-from .resena import Resena
-
-__all__ = ["Usuario", "Resena", "db"]
